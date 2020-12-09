56caa0c1b19667c5b73781db75f9f8e004289a60
Muhammad Waqas Baig <muhammad.waqas.baig.2020@gmail.com>
2020-12-09 13:35:47 +0500

    Working

---
+++ ScrapWeb/Default.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ScrapWeb
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
+
+            //HtmlAgilityPack.HtmlDocument doc = web.Load("https://www.paci.gov.kw/");
+            //string PACI = "";
+            //var data = doc.DocumentNode.SelectNodes("//div");
+
+
+            HtmlAgilityPack.HtmlDocument doc = web.Load("https://homeshopping.pk/categories/Fresh-Produce-Karachi/");
+            string itemName = "";
+            string price = "";
+            int i = 0;
+            var toScrap = doc.DocumentNode.SelectNodes("//div[contains(@class, \'ProductsList\')]");
+            foreach (var item in toScrap)
+            {
+                Response.Write(item.InnerHtml);
+
+                itemName = item.SelectNodes("//div[contains(@class, \'padx8\')]/h5/a")[i].InnerText;
+                price = item.SelectNodes("//div[contains(@class,\'ActualPrice\')]")[i].InnerText;
+                Response.Write("<br/> <h1>One Item Extraction ... </h1> <br/> <div><h2>" + itemName + "</h2><br/><h3>" + price + "</h3>");
+                i += 1;
+            }
+        }
+    }
+}
---
+++ ScrapWeb/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ScrapWeb")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ScrapWeb")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2d785e9f-bba7-41ea-b027-559a9f33869d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
